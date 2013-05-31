70c3e311acd1c69b13d451371d293069975939a9
Rodrigo Dias Ferreira <rodrigodias@gmx.de>
2013-05-31 13:09:23 -0300

    Adding routes to signin and signout and named routes
    Signed-off-by: Rodrigo Dias Ferreira <rodrigodias@gmx.de>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :session, only: [:new, :create, :destroy]
   root to: 'static_pages#home'
+  #Named routes to user model
   match '/signup',  to: 'users#new',            via: 'get'
+  #Named routes to session controller
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  #Routes to static_pages
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'

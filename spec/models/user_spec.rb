ecd00a77e12a244e34363334955778e3112e0dda
Rodrigo Dias Ferreira <rodrigodias@gmx.de>
2013-06-03 22:02:46 -0300

    Adding test for remember_token
    Signed-off-by: Rodrigo Dias Ferreira <rodrigodias@gmx.de>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -115,5 +115,10 @@ describe User do
       expect(@user.reload.email).to eq mixed_case_email.downcase
     end
   end
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
     
 end

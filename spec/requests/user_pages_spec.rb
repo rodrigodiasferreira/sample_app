89ac9e54a4855ccca06b273267afa2b607717cd2
Rodrigo Dias Ferreira <rodrigodias@gmx.de>
2013-06-05 00:21:27 -0300

    Adding sign out feature
    Signed-off-by: Rodrigo Dias Ferreira <rodrigodias@gmx.de>

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -57,7 +57,7 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-      
+
       describe "after saving the user" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }

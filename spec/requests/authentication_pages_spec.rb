4c98ca25db2480d3335769baab4ab0947db5302b
Rodrigo Dias Ferreira <rodrigodias@gmx.de>
2013-06-08 19:06:37 -0300

    Add cucumber test behavior-driven and Matchers (which did not work)
    Signed-off-by: Rodrigo Dias Ferreira <rodrigodias@gmx.de>

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -24,6 +24,7 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      #should have_error_message('Invalid')
       
       describe "after visiting another page" do
         before { click_link "Home" }
@@ -39,6 +40,7 @@ describe "Authentication" do
         fill_in "Password", with: user.password
         click_button "Sign in"
       end
+      #before valid_signin(user)
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }

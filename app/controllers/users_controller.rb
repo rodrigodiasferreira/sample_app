990f0956efa9eb5b0a6eb61d91a6a11ce8fab749
Rodrigo Dias Ferreira <rodrigodias@gmx.de>
2013-06-12 23:02:13 -0300

    Redirecting to root_page in case that signed_in users try to edit others users' profiles
    Signed-off-by: Rodrigo Dias Ferreira <rodrigodias@gmx.de>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   
   def index
   end
@@ -24,11 +25,11 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       sign_in @user
@@ -51,6 +52,11 @@ class UsersController < ApplicationController
     # Before filters
     def signed_in_user
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
   
 end

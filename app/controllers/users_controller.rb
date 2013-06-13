e2c2a840064eda9e13418c14a4ba51e40268927e
Rodrigo Dias Ferreira <rodrigodias@gmx.de>
2013-06-12 21:49:28 -0300

    Implementing authorization
    Signed-off-by: Rodrigo Dias Ferreira <rodrigodias@gmx.de>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :signed_in_user, only: [:edit, :update]
+  
   def index
   end
 
@@ -45,6 +46,11 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
   
 end

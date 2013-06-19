c660eb1a7c43a61a9bdaa59f2b4551c7e0413258
Rodrigo Dias Ferreira <rodrigodias@gmx.de>
2013-06-18 23:32:04 -0300

    Adding Delete by admin feature
    Signed-off-by: Rodrigo Dias Ferreira <rodrigodias@gmx.de>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
-  
+  before_action :admin_user,     only: :destroy
+
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -41,6 +42,9 @@ class UsersController < ApplicationController
   end
   
   def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
   end
   
   private
@@ -61,6 +65,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
   
 end

c6f45f8f6f20216ac05be5e8031444e3b98b55a4
Rodrigo Dias Ferreira <rodrigodias@gmx.de>
2013-06-19 21:48:34 -0300

    Exercise 9.6.6 solved
    Signed-off-by: Rodrigo Dias Ferreira <rodrigodias@gmx.de>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
+  before_action :redirect_to_root_if_not_signed_in,     only: [:new, :create]
+  
 
   def index
     @users = User.paginate(page: params[:page])
@@ -69,6 +71,10 @@ class UsersController < ApplicationController
     
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+
+    def redirect_to_root_if_not_signed_in
+      redirect_to(root_path) if signed_in?
     end
   
 end

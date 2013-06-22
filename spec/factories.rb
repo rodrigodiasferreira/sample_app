80effcf851a0487f23741970714c368342e76776
Rodrigo Dias Ferreira <rodrigodias@gmx.de>
2013-06-21 21:58:14 -0300

    Adding constraints to models users and microposts associations
    Signed-off-by: Rodrigo Dias Ferreira <rodrigodias@gmx.de>

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,20 +9,8 @@ FactoryGirl.define do
       admin true
     end
   end
-end
-
-FactoryGirl.define do
-  factory :user do
-    sequence(:name)  { |n| "Person #{n}" }
-    sequence(:email) { |n| "person_#{n}@example.com"}
-    password "foobar"
-    password_confirmation "foobar"
-
-    factory :admin do
-      admin true
-    end
-  end
-
+  
+  #Microposts
   factory :micropost do
     content "Lorem ipsum"
     user

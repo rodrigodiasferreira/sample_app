6623666c8c32d7823ec509f2d3b8f436ca17c199
Rodrigo Dias Ferreira <rodrigodias@gmx.de>
2013-07-23 22:09:20 -0300

    Adding follow/unfollow button
    Signed-off-by: Rodrigo Dias Ferreira <rodrigodias@gmx.de>

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ SampleApp::Application.routes.draw do
   end
   resources :session, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root to: 'static_pages#home'
   #Named routes to user model
   match '/signup',  to: 'users#new',            via: 'get'
